ef29375d2205c3028398e46c0c187d0e1113d2eb
caoliduo <894871274@qq.com>
2021-01-07 21:14:55 +0800

    多线程安全队列

---
+++ UnsafeList.java
@@ -0,0 +1,11 @@
+package Thread;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnsafeList {
+    public static void main(String[] args){
+        List<String> list=new ArrayList<>();
+        //for()
+    }
+}
