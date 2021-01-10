fbc1e543e6fbcf7d4a2aa7b8ad4704aaa0513dd9
caoliduo <894871274@qq.com>
2021-01-10 20:10:59 +0800

    stream流

--- Test.java
+++ Test.java
@@ -1,85 +1,28 @@
-package Thread.Bq;
+package Thread.MyStream;
 
-import java.util.Collections;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingDeque;
-import java.util.concurrent.TimeUnit;
+import java.util.Arrays;
+import java.util.List;
 
 public class Test {
-    public static void main(String[] args) throws InterruptedException {
-        Test t=new Test();
-        t.test3();
-    }
-    /*
-    * 抛出异常
-    * */
-    public void Test(){
-        ArrayBlockingQueue blockingQueue=new ArrayBlockingQueue(3);
+    public static void main(String[] args) {
+        User u1=new User(1,"a",21);
+        User u2=new User(2,"b",22);
+        User u3=new User(3,"c",23);
+        User u4=new User(4,"d",24);
+        User u5=new User(5,"e",25);
+        //集合是存储
+        List<User> list= Arrays.asList(u1,u2,u3,u4,u5);
 
-        System.out.println(blockingQueue.add("a"));
-        System.out.println(blockingQueue.add("b"));
-        System.out.println(blockingQueue.add("c"));
-        //System.out.println(blockingQueue.add("d"));
-        //java.lang.IllegalStateException: Queue full抛出异常
-        System.out.println("----------------");
-
-
-        System.out.println(blockingQueue.remove());
-        System.out.println(blockingQueue.remove());
-        System.out.println(blockingQueue.remove());
-        //System.out.println(blockingQueue.remove());
-        //java.util.NoSuchElementException抛出异常
+        //计算使用stream流
+        //链式编程
+        list.stream().filter(u->{return u.getId()%2==0;}).forEach(System.out::println);
+        list.stream().filter(u->{return u.getAge()>23;})
+                .map(user -> user.getName()
+                        .toUpperCase())
+                .sorted((uu1,uu2)->{return uu2.compareTo(uu1);})
+                .limit(1)
+                .forEach(System.out::println);
 
     }
-    public  static  void Test1(){
-        ArrayBlockingQueue blockingQueue=new ArrayBlockingQueue(3);
 
-        System.out.println(blockingQueue.offer("a"));
-        System.out.println(blockingQueue.offer("b"));
-        System.out.println(blockingQueue.offer("c"));
-       // System.out.println(blockingQueue.offer("d"));//false，不抛异常
-        System.out.println("-------------");
-        System.out.println(blockingQueue.peek());
-        System.out.println(blockingQueue.poll());
-        System.out.println(blockingQueue.poll());
-        System.out.println(blockingQueue.poll());//null,不抛异常
-      //  System.out.println(blockingQueue.element());会抛出异常
-        System.out.println(blockingQueue.peek());
-    }
-    /*
-    * 等待，阻塞（一直等待）
-    * */
-    public static  void test2() throws InterruptedException {
-        //队列的大小
-        ArrayBlockingQueue blockingQueue=new ArrayBlockingQueue(3);
-        //一直阻塞
-        blockingQueue.put("a");
-        blockingQueue.put("B");
-        blockingQueue.put("C");
-        //blockingQueue.put("d");//队列没有位置了，他就会一直阻塞在这个语句处
-        System.out.println(blockingQueue.take());
-        System.out.println(blockingQueue.take());
-        System.out.println(blockingQueue.take());
-      //  System.out.println(blockingQueue.take());//没有元素，他就会一直阻塞到这里，等待取出。并且没有异常抛出
-
-    }
-    /*
-    * 等待，阻塞（等待超时)
-    * */
-    public static void test3() throws InterruptedException {
-        //队列的大小
-        ArrayBlockingQueue blockingQueue=new ArrayBlockingQueue(3);
-
-        blockingQueue.offer("a");
-        blockingQueue.offer("b");
-        blockingQueue.offer("c");
-        //blockingQueue.offer("d",2, TimeUnit.SECONDS);//等待超过两秒就退出
-
-        System.out.println("-----------");
-        System.out.println(blockingQueue.poll());
-        System.out.println(blockingQueue.poll());
-        System.out.println(blockingQueue.poll());
-        System.out.println(blockingQueue.poll());
-    }
 }
-
