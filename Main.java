3e7faffb0d7c482bf0f4d594eeb81300a028f565
duoduo <894871274@qq.com>
2021-03-18 23:00:40 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,23 +1,55 @@
-package Try.stringlen;
+package Try.nm;
 
+
+import java.util.Scanner;
 import java.util.*;
 public class Main{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        String str=sc.nextLine();
-        StringBuffer len= new StringBuffer("");
-        StringBuffer last= new StringBuffer("");
-        for(int i=0;i<str.length();i++){
+        String s=sc.nextLine();
+        String[] str =s.split(" ");
+        int n=Integer.parseInt(str[0]);
+        int m=Integer.parseInt(str[1]);
+        ArrayList<ArrayList<Integer>> endlist=new ArrayList<>();
+        ArrayList<Integer> list=new ArrayList<>();
 
-            if(str.charAt(i)<='9'&&str.charAt(i)>='0'){
-                len.append(str.charAt(i));
-            }
-            else{
-                if(len.length()>last.length()){
-                    last=len;
+        for(int i=1;i<=n/2+1;i++){
+            int tar=m;
+            list.add(i);
+            int j=i;
+            tar-=j;
+
+            while(tar>0){
+
+                if(tar<=n){//把单独成为内容的放进去
+
+                    if(!list.contains(j))list.add(j);
+                    if(!list.contains(tar))list.add(tar);
+                    if(!endlist.contains(list)){
+                        endlist.add(new ArrayList<>(list));
+                    }
+                    list.clear();
+                    break;
                 }
+                j++;
+                tar-=j;
+            }
+            if(tar==0){
+                list.add(i-1);
+                endlist.add(new ArrayList<>(list));
+                tar=m;
+                list.clear();
             }
         }
-        System.out.println(last.toString());
+        if(m<=n){
+            list.add(m);
+            endlist.add(list);
+        }
+        for(ArrayList<Integer> a:endlist){
+            for(Object i:a){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
     }
 }
