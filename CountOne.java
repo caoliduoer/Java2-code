22917390b371a98afe1990784c8348657b3eaa8e
caoliduo <894871274@qq.com>
2021-06-10 16:57:17 +0800

    计算1的个数

---
+++ CountOne.java
@@ -0,0 +1,15 @@
+public class CountOne {
+    public int NumberOf1(int n) {
+
+        int count=0;
+        while(n!=0){
+            n=n&(n-1);
+            count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
