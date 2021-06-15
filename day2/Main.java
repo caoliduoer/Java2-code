f9a7cf8b2cfa243326f06d0bddeefccc8ab8518b
caoliduo <894871274@qq.com>
2021-06-15 21:21:04 +0800

    树的子结构

---
+++ day2/Main.java
@@ -0,0 +1,30 @@
+package day2;
+ class TreeNode {
+ int val = 0;
+ TreeNode left = null;
+ TreeNode right = null;
+
+ public TreeNode(int val) {
+ this.val = val;
+
+ }
+
+ }
+public class Main {
+    public boolean HasSubtree(TreeNode root1,TreeNode root2) {
+        if(root1==null||root2==null){
+            return false;
+        }
+        return isSubTree(root1,root2)||HasSubtree(root1.left,root2)||HasSubtree(root1.right,root2);
+
+    }
+    public boolean isSubTree(TreeNode root1,TreeNode root2){
+        if(root2==null){
+            return true;
+        }
+        if(root1==null){
+            return false;
+        }
+        return root2.val==root1.val&&isSubTree(root1.left,root2.left)&&isSubTree(root1.right,root2.right);
+    }
+}
